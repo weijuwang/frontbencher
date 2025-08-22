6ce3b39a9e580dda37fe8fb36f4e311c0d6bf370
Weiju Wang <weijuw@outlook.com>
2025-08-21 20:27:57 -0700

    Docs for card module

---
+++ src/card.rs
@@ -0,0 +1,20 @@
+/// A bitfield of a collection of cards.
+type Collection = u64;
+
+/// A card's rank.
+enum Rank {
+    Ace = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Joker = 14
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 /// Methods for operating on cards and collections of cards.
 ///
 /// ## Internal representation of card collections
+/// The information below is not necessary to use the library but is useful to understand how the
+/// data is being represented.
 ///
 /// Each card is given a unique **index number**. A collection of cards is represented as a
 /// [Collection] -- a type alias for [u64]. When a card is present in a Collection, the bit at the
@@ -17,11 +19,25 @@
 /// The low four bits -- or the lowest place value hex digit -- of any index number are the card's
 /// [Rank]. There are 14 ranks of cards from Ace to Joker.
 ///
+/// Ranks can be directly compared by casting them to an integer.
+///
 /// #### Color
 /// The bit at place value 2^5 = `0b10_0000` represents the card's color. `0` means black; `1` means
 /// red.
 ///
+/// #### Suit
+/// The bit at place value 2^4 = `0b1_0000` represents the card's suit.
 ///
+/// For black cards, `0` means clubs or a black joker; `1` means spades.
+///
+/// For red cards, `0` means diamonds or a red joker; `1` means hearts.
+///
+/// When comparing two cards' suit, both the color and the suit must be compared because the meaning
+/// of the suit bit is dependent on the color. For reference:
+/// - **Clubs/black joker:** `00xxxx` binary or `0x` hex
+/// - **Spades:** `01xxxx` binary or `1x` hex
+/// - **Diamonds/red joker:** `10xxxx` binary or `2x` hex
+/// - **Hearts:** `11xxxx` binary or `3x` hex
 mod card;
 
 fn main() {
